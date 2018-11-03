3461f7bff6dd1d5117a8ba84c709c72cd5bca208
C Howe <brook.theis@gmail.com>
2018-11-03 13:31:49 -0600

    Map displayed on ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,18 +12,23 @@ $.ajax({
     method: "GET"
 }).done(function (response) {
     console.log(response);
-
-})
-
     //brook google maps api call
     var map;
     function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
+        var uluru = {lat: -25.344, lng: 131.036};
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: uluru});
+    
+        var marker = new google.maps.Marker({position: uluru, map: map});
       }
-//   initMap();
+    
+      initMap();
+
+})
+
+    
+    
+
 
 
 
