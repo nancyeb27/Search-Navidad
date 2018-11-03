1deff62ada8a7ef844e235ad9fd0178fa9426e72
Nancyb <nancyeb27@yahoo.com>
2018-11-03 13:31:47 -0600

    update nb js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,32 @@
-$(document).ready(function () {
 
 
+$("#search-input").on("click", function (event) {
+    event.preventDefault();
 
+    // This line grabs the input from the textbox
+    var newEvent = $("#Search").val().trim();
 
-
-
-    var x = $(this).attr("show-button");
-    console.log(x);
-
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/upcoming_events?zip=80209&radius=100.0&text=christmas&key=37216631b5fb603c2f5a67701a1d";
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/upcoming_events?zip=" + userZip +"&radius=100.0&text=christmas&key=37216631b5fb603c2f5a67701a1d";
     console.log(queryURL);
+    var userZip = $("#zip").val().trim().
 
     $.ajax({
         url: queryURL,
         method: "GET"
+
     }).done(function (response) {
 
-        
-        console.log(response);
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) {
+            console.log(response);
+        }
+
     })
+              
 
 })
+
+
+
+    
