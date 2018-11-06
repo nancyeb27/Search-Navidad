e7e953a0d120f2024f424b5ea4f4c1dc703200c4
C Howe <brook.theis@gmail.com>
2018-11-05 17:56:21 -0700

    Updated meetups url, maps moved to correct location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,18 +4,18 @@
 $("#search-input").on("click", function (event) {
     event.preventDefault();
 
-   var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/upcoming_events?zip=" + userZip +"&radius=100.0&text=christmas&key=37216631b5fb603c2f5a67701a1d";
+    var userZip = $("#zip").val().trim()
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=" + userZip + "&text=christmas&page=20&key=37216631b5fb603c2f5a67701a1d";
     console.log(queryURL);
-    var userZip = $("#zip").val().trim().
+    var userZip = $("#zip").val().trim()
 
     $.ajax({
         url: queryURL,
         method: "GET"
 
     }).done(function (response) {
+        console.log(response);
 
-        var results = response.data;
-      
         //brook google maps api call
         var map;
         function initMap() {
@@ -26,9 +26,9 @@ $("#search-input").on("click", function (event) {
           }
         initMap();
       
-        for (var i = 0; i < results.length; i++) {
-            console.log(response);
-        }
+        // for (var i = 0; i < results.length; i++) {
+        //     console.log(response);
+        // }
 
     })
               
