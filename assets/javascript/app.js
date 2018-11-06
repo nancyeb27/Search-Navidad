1ae1594f76ca83f08c554fa9ce4d1435ad867783
C Howe <brook.theis@gmail.com>
2018-11-05 19:00:29 -0700

    fixed 'undefined' bug in forloop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,24 +3,20 @@
 
 $("#search-input").on("click", function (event) {
     event.preventDefault();
-    
-    var userZip = $("#zip").val().trim()
 
-    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=" + userZip + "&text=christmas&radius=10&page=20&key=37216631b5fb603c2f5a67701a1d";
-       
+    var userZip = $("#zip").val().trim()
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/2/open_events?&sign=true&photo-host=public&zip=" + userZip + "&text=christmas&page=20&key=37216631b5fb603c2f5a67701a1d";
     console.log(queryURL);
-  
+    var userZip = $("#zip").val().trim()
 
     $.ajax({
         url: queryURL,
         method: "GET"
 
-    }).then(function (response) {
-        console.log(response)
-
-        var results = response.results;
-        console.log(results);
-             
+    }).done(function (response) {
+        console.log(response);
+        var results = response.results
+         
         //brook google maps api call
         var map;
         function initMap() {
@@ -29,19 +25,26 @@ $("#search-input").on("click", function (event) {
             document.getElementById('map'), {zoom: 4, center: uluru});
           var marker = new google.maps.Marker({position: uluru, map: map});
           }
-        initMap();
       
-       
+        for (var i = 0; i < results.length; i++) {
 
-        // for (var i = 0; i < results.length; i++) {
-        //     var name = results[i].name;
-        //     var description = results[i].description;
+            var lat
+            
+            if (!results[i].venue) {
+                
 
-        //  $("#name").prepend(name);
-        //  $("#description").append(description);
-        // 
+            }else{
+            
+                lat = results[i].venue.lat
+            }
 
-    })          
+            console.log("Test:" + lat);
+            
+            initMap();
+        }
+
+    })
+              
 
 })
 
